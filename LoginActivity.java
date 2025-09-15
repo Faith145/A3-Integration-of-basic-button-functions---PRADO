01291a005002e040322353efbfa892ce6fbdf64a
ARWEN FAITH O. PRADO 2C <arwenfaith.prado@hcdc.edu.ph>
2025-09-15 23:04:46 +0800

    Create LoginActivity.java

---
+++ LoginActivity.java
@@ -0,0 +1,27 @@
+package com.example.a3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button backToMainBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        backToMainBtn = findViewById(R.id.backToMainBtn);
+        backToMainBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
